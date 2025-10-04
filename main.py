2b47325d6e49d680535797f5fd77ad6866db9120
noahleavi11 <noahjlvtt@gmail.com>
2025-10-04 16:14:09 -0500

    minor refactoring

--- main.py
+++ main.py
@@ -6,20 +6,26 @@ def get_book_text(filepath):
         file_contents = f.read()
         return file_contents
 
+
+def print_func(book, text, char_dict):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book}")
+    print("----------- Word Count ----------")
+    get_num_words(text)
+    print("--------- Character Count -------")
+    for dic in char_dict:
+        char, num = dic.values()
+        if char.isalpha():
+            print(f"{char}: {num}")
+
+
 def main():
     if len(sys.argv) < 2:
         print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
     text = get_book_text(sys.argv[1])
-    print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {sys.argv[1]}")
-    print("----------- Word Count ----------")
-    get_num_words(text)
-    print("--------- Character Count -------")
     text_dict = get_num_characters(text)
     sorted_chars = report(text_dict)
-    for dic in sorted_chars:
-        char, num = dic.values()
-        if char.isalpha():
-            print(f"{char}: {num}")
+    print_func(book=sys.argv[1], text=text, char_dict=sorted_chars)
+
 main()
